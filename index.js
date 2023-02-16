6c2eb34368161378708b119dc8b4a26bc709996d
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2023-02-16 11:06:20 -0600

    Safely create JWT token

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Demoing JWTs...");
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+// can use jwt.sign(payload, secret) // create a JWT
+
+// JWT_SECRET = "here's a string that everyone can see in my git repo!!!"  // BAD don't do this
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// payload => going to be encrypted, user data (name, id, pw, sub, iat = time when generated, exp)
+// lots of claims about user could go in here
+const user = {
+    "name": "Ada Lovelace",
+    "occupation": "theoretical computer scientist",
+    "lived": "1815-1852"
+}
+
+const token = jwt.sign(user, JWT_SECRET);
+console.log(token);
